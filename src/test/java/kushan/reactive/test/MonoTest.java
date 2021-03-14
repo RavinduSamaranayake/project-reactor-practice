721678ee6aea60f0faccf05fead5dec046028553
Ravindu95 <kushan.r@ustocktrade.com>
2021-03-14 20:53:25 +0530

    1.Subscribing to Mono

--- src/test/java/kushan/reactive/test/MonoTest.java
+++ src/test/java/kushan/reactive/test/MonoTest.java
@@ -3,12 +3,38 @@ package kushan.reactive.test;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+/*
+  Reactive Streams
+  1. Asynchronous
+  2. Non-blocking
+  3. Backpressure
+  Publisher <- (subscribe) Subscriber
+  Subscription is created
+  Publisher (onSubscribe with the subscription) -> Subscriber
+  Subscription <- (request N) Subscriber
+  Publisher -> (onNext) Subscriber
+  until:
+  1. Publisher sends all the objects requested.
+  2. Publisher sends all the objects it has. (onComplete) subscriber and subscription will be canceled
+  3. There is an error. (onError) -> subscriber and subscription will be canceled
+ */
 public class MonoTest {
     //Creating the Logger object
     Logger log = LoggerFactory.getLogger(this.getClass());
     @Test
-    public void test(){
-        log.info("Project Setup Complete..");
+    public void monoSubscriber(){
+        String name = "Kushan Ravindu";
+        Mono<String> mono = Mono.just(name)
+                .log();
+        mono.subscribe();
+
+        log.info("-------------------test the code using reactor-test step verifier---------------------------");
+
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
     }
 }
