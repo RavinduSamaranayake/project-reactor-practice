d8ae70cc2084a564ce781c66722a1eea5c72e8b4
Ravindu95 <kushan.r@ustocktrade.com>
2021-03-14 20:00:38 +0530

    Initial commit

---
+++ src/test/java/kushan/reactive/test/MonoTest.java
@@ -0,0 +1,14 @@
+package kushan.reactive.test;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MonoTest {
+    //Creating the Logger object
+    Logger log = LoggerFactory.getLogger(this.getClass());
+    @Test
+    public void test(){
+        log.info("Project Setup Complete..");
+    }
+}
